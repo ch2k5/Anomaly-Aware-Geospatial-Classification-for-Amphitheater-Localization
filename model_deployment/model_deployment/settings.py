d3c7adf3e49bdbd6b6103ac7aeca7706e46c9832
abdelaliMokhati <abdelali.mokhati@ensia.edu.dz>
2026-05-01 21:52:00 +0100

    feat: link backend

--- model_deployment/model_deployment/settings.py
+++ model_deployment/model_deployment/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "*"]
 
 extra_hosts = os.environ.get("DJANGO_ALLOWED_HOSTS", "")
 if extra_hosts:
